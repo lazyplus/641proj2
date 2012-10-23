616d48f15a330f5a39b4ed32a3624b77ce534817
Hanshi <lhs860425@gmail.com>
2012-10-23 17:41:43 -0400

    commentted py

--- webserver.py
+++ webserver.py
@@ -30,36 +30,26 @@ def rd_getrd(p):
     #2. Connect to the routing daemon on port p
     request_line = "GETRD " + str(len(request_obj)) + " " + request_obj
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #sock.setblocking(0)
     print "local port: " + str(p)
     local_port = p
     sock.connect((rd_host, local_port))
-    #print sock
     #3. Do GETRD <object-name>
     sock.send(request_line) 
     print "Request: " + request_line
     #4. Parse the response from the routing daemon
-    rd_response_buff = ""
-    # ret = rd_response_buff.find("\r\n")
-    # while (ret == -1):
     recv_buff = sock.recv(1024)
     print "APP Get " + recv_buff
-    # rd_response_buff = rd_response_buff + recv_buff
-    print recv_buff
+    # split the received string by whitespace
     recv_arr = re.split("\s+", recv_buff)
     if len(recv_arr) == 2:
         return "Content not found"
-    rd_response = recv_arr[2]
-    # rd_response = rd_response_buff[:ret+2]
-    # rd_response_buff = rd_response_buff[ret+2:]
-    print rd_response    
-    #rd_response = "OK 25 http://localhost:9999/static/index.html"
-    # L = rd_response.split(' ')    
     L = recv_arr
     if (L[0] == "OK"):
+        # response is OK, download the file.
         print L[2]
         result = urllib.urlopen(L[2])
         file_content = result.read()
+        # compose a HTTP response to return the file to the browser
         response = flask.make_response(file_content)
         for header_line in str(result.info()).split("\r\n"):
             if (header_line != ""):
@@ -68,28 +58,26 @@ def rd_getrd(p):
                 value = pair[1]
                 response.headers[key] = value
         if (L[2].find("http://localhost:") == -1):
-            # file not exist in local host
+            # if file is not from the local host, save it to disk and add a entry in the local route daemon
             file_name = request_obj
             local_file = open("static/" + hashlib.sha256(file_name).hexdigest(), "w")
             local_file.write(file_content)
             local_file.close()
+            # add entry to local route daemon
             request_line = "ADDFILE " + str(len(file_name)) + " " + file_name + " " + str(len("/static/" + hashlib.sha256(file_name).hexdigest())) + " /static/" + hashlib.sha256(file_name).hexdigest()
             sock.send(request_line)
             print "Send:" + request_line
-            # sock.recv(1024)
             recv_buff = sock.recv(1024)
             print "APP Get " + recv_buff
+            # check if the response is OK
             if(recv_buff.find("OK") == -1):
                 return "Add local file failed"
+        sock.close()
         return response
     else:
+        # response is not OK
+        sock.close()
         return "Content not available"
-    #4 a)If response is OK <URL>, the open the URL
-    #4 b) If response is 404, then show that content is not available
-    #### You may factor out things from here and rd_getrd() function and form a separate sub-routine
-    
-    #return "Unimplemented"
-            
 
 @app.route('/rd/addfile/<int:p>', methods=["POST"])
 def rd_addfile(p):
@@ -101,32 +89,20 @@ def rd_addfile(p):
     #3. Save the file in the static directory under the sha256sum name and compute the relative path
     f.save("static/" + hashlib.sha256(file_name).hexdigest())
     #4. Connect to the routing daemon on port p
-    request_line = "ADDFILE " + str(len(file_name)) + " " + file_name + " " + str(len("/static/" + hashlib.sha256(file_name).hexdigest())) + " /static/" + hashlib.sha256(file_name).hexdigest() + "\r\n"
+    request_line = "ADDFILE " + str(len(file_name)) + " " + file_name + " " + str(len("/static/" + hashlib.sha256(file_name).hexdigest())) + " /static/" + hashlib.sha256(file_name).hexdigest()
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((rd_host, local_port))
     #5. Do ADDFILE <object-name> <relative-path> 
     sock.send(request_line)
     #6. Based on the response from the routing daemon display whether the object has been successfully uploaded/added or not 
-    rd_response_buff = ""
-    ret = rd_response_buff.find("\r\n")
-    while (ret == -1):
-        recv_buff = sock.recv(1024)
-        rd_response_buff = rd_response_buff + recv_buff
-        ret = rd_response_buff.find("\r\n")
-        if (len(rd_response_buff) > 10*1024):
-            print "No EOF found"
-            exit()
-    rd_response = rd_response_buff[:ret+2]
-    rd_response_buff = rd_response_buff[ret+2:]
-    print rd_response
-    L = rd_response.split(' ')    
-    if (L[0] == "OK"):
-        print L[1]
+    print "Send:" + request_line
+    recv_buff = sock.recv(1024)
+    print "APP Get " + recv_buff
+    sock.close()
+    if(recev_buff.find("OK") == -1):
+        return "Add local file failed"
     else:
-        print "Content not available"
-    sock.close()    
-    return "Unimplemented"
-
+        return "File added"
 
 @app.route('/rd/<int:p>/<obj>', methods=["GET"])
 def rd_getrdpeer(p, obj):
@@ -137,27 +113,17 @@ def rd_getrdpeer(p, obj):
     #2. Connect to the routing daemon on port p
     request_line = "GETRD " + str(len(request_obj)) + " " + request_obj
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #sock.setblocking(0)
     print "Conn: " + rd_host + ":" + str(local_port)
     sock.connect((rd_host, local_port))
-    #print sock
     #3. Do GETRD <object-name>
     sock.send(request_line) 
     print "Request: " + request_line
     #4. Parse the response from the routing daemon
-    rd_response_buff = ""
-    # ret = rd_response_buff.find("\r\n")
-    # while (ret == -1):
     recv_buff = sock.recv(1024)
-    # rd_response_buff = rd_response_buff + recv_buff
     print recv_buff
     recv_arr = re.split("\s+", recv_buff)
     rd_response = recv_arr[2]
-    # rd_response = rd_response_buff[:ret+2]
-    # rd_response_buff = rd_response_buff[ret+2:]
     print rd_response    
-    #rd_response = "OK 25 http://localhost:9999/static/index.html"
-    # L = rd_response.split(' ')    
     L = recv_arr
     if (L[0] == "OK"):
         print L[2]
@@ -169,8 +135,10 @@ def rd_getrdpeer(p, obj):
                 key = pair[0]
                 value = pair[1]
                 response.headers[key] = value
+        sock.close()
         return response
     else:
+        sock.close()
         return "Content not available"
 
 
